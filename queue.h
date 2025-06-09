cd4f6d89c59e1408f6cf239e2b71e04455876a15
MooTo2002 <xotmd1002@gmail.com>
2025-06-09 19:22:18 +0900

    06/09 queue.h
    PDF 예제 복사. 수정하면 안됨.

--- queue.h
+++ queue.h
@@ -1,9 +1,14 @@
-// queue.h
+// queue.h 수정XX
 #pragma once
 #include "qtype.h"
 
 Queue* init(void);
+void release(Queue* queue);
+
 Node* nalloc(Item item);
+void nfree(Node* node);
+Node* nclone(Node* node);
+
 Reply enqueue(Queue* queue, Item item);
 Reply dequeue(Queue* queue);
 Queue* range(Queue* queue, Key start, Key end);
